96f80732da51dae5fd6426fbcba2b59be6881ce3
Ahmed Gedi <agedi98@gmail.com>
2024-07-12 10:37:56 -0400

    Initial Commit

---
+++ RoomsToGo/RoomsToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsToGoApp.swift
+//  RoomsToGo
+//
+//  Created by Ahmed Gedi on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RoomsToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
